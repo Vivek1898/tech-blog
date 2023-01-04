90f238ee5d03c1d335d990fc0fbb049025a2e6be
Vivek Singh <viveksingh27795@gmail.com>
2023-01-04 14:40:16 +0530

    env

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const withCSS = require('@zeit/next-css')
+require('dotenv').config()
+module.exports =withCSS({ 
+    publicRuntimeConfig :{
+        APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
+        API_DEVELOPMENT:process.env.NEXT_PUBLIC_API,
+        PRODUCTION: process.env.NEXT_PUBLIC_PRODUCTION,
+        DOMAIN_DEVELOPMENT: process.env.NEXT_PUBLIC_DOMAIN,
+        DISQUS_SHORTNAME: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
+        GOOGLE_CLIENT_ID:process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+        FB_APP_ID: process.env.NEXT_PUBLIC_FB_APP_ID
+
+}
+})

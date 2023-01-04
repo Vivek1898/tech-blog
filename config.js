247c29c8e7aa4a395eb8d7054abc4e5ee1fbad03
Vivek Singh <viveksingh27795@gmail.com>
2023-01-04 14:45:40 +0530

    env

--- config.js
+++ config.js
@@ -1,15 +1,11 @@
 import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
-export const API = publicRuntimeConfig.PRODUCTION
-? publicRuntimeConfig.API_DEVELOPMENT
-: publicRuntimeConfig.API_DEVELOPMENT;
+export const API = publicRuntimeConfig.API_DEVELOPMENT;
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
-export const DOMAIN = publicRuntimeConfig.PRODUCTION
-? publicRuntimeConfig.API_DEVELOPMENT
-: publicRuntimeConfig.API_DEVELOPMENT;
+export const DOMAIN = publicRuntimeConfig.API_DEVELOPMENT;
 
 export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
 export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME;

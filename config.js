45dc9da715c8b8d03c2828c1ada47e44235eb0e2
Vivek Singh <viveksingh27795@gmail.com>
2023-01-06 03:16:56 +0530

    se

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ export const API = publicRuntimeConfig.API_DEVELOPMENT;
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
-export const DOMAIN = publicRuntimeConfig.API_DEVELOPMENT;
+export const DOMAIN = publicRuntimeConfig.DOMAIN_DEVELOPMENT;
 
 export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
 export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME;
